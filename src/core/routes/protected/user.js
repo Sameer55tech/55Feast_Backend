3855abe91047d0687fed4a06bf5118018b210b0b
Sameer55tech <sameer.srivastava@fiftyfivetech.io>
2023-07-03 19:16:56 +0530

    modified routes

--- src/core/routes/protected/user.js
+++ src/core/routes/protected/user.js
@@ -6,11 +6,11 @@ const router = express.Router();
 router.get("/all", userController.getAllUsers);
 router.get("/location/all", userController.getUserByLocation);
 router.get("/", userController.getUser);
-router.get("/all/joined", userController.getJoinedUsers);
+router.post("/all/joined", userController.getJoinedUsers);
 router.post("/insert", userController.insertUser);
 router.patch("/update", userController.updateUserPool);
 router.delete("/delete", userController.deleteUser);
-router.get("/all/invite", userController.getNotJoinedUsers);
+router.post("/all/invite", userController.getNotJoinedUsers);
 router.post("/invite", userController.inviteUser);
 
 export default router;
