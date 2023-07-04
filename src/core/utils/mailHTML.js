d82a74cfc55bc9cf5ec3fdd89b78c5e64b552ac0
Sameer55tech <sameer.srivastava@fiftyfivetech.io>
2023-07-04 12:01:54 +0530

    updated mail html

--- src/core/utils/mailHTML.js
+++ src/core/utils/mailHTML.js
@@ -9,34 +9,34 @@ const mailHTML = (htmlResponse) => {
     <style>
       /* Inline CSS */
       body {
-        font-family: Arial, sans-serif;
-        line-height: 1.5;
-        color: #333333;
+        font-family: Arial, sans-serif !important;
+        line-height: 1.5 !important;
+        color: #333333 !important;
       }
       .header {
-        background-color: #f5f5f5;
-        padding: 20px;
-        text-align: center;
+        background-color: #f5f5f5 !important;
+        padding: 20px !important;
+        text-align: center !important;
       }
       .header h1 {
-        margin: 0;
+        margin: 0 !important;
       }
       .body {
-        padding: 20px;
-        text-align: center;
+        padding: 20px !important;
+        text-align: center !important;
       }
       .button {
-        display: inline-block;
+        display: inline-block !important;
         text-decoration: none !important;
-        background-color: #ef5d36;
+        background-color: #ef5d36 !important;
         color: #fff !important;
-        padding: 10px 20px;
-        border-radius: 4px;
+        padding: 10px 20px !important;
+        border-radius: 4px !important;
       }
       .button:hover {
-        background-color: #fff;
+        background-color: #fff !important;
         border-color: #ef5d36 !important;
-        color: #ef5d36;
+        color: #ef5d36 !important;
       }
     </style>
   </head>
@@ -51,7 +51,7 @@ const mailHTML = (htmlResponse) => {
         features and services we offer.
       </p>
       <p>Click the button below to visit our website:</p>
-      <p><a class="button" href="https://five5feast-3nyz.onrender.com/">Visit Website</a></p>
+      <p><a class="button" href="https://five5feast-3nyz.onrender.com/">Join 55Feast</a></p>
       <p>We look forward to having you as our valued user.</p>
       <p>Best regards,</p>
       <p>Your Company</p>
