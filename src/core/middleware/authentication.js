ae4877cc81cc37d3d696a742a41d075949d50cb4
Sameer Srivastava <sameer.srivastava@fiftyfivetech.io>
2023-07-19 15:20:22 +0530

    minor fix

--- src/core/middleware/authentication.js
+++ src/core/middleware/authentication.js
@@ -4,12 +4,11 @@ import { jwt, messageResponse, onError, sendResponse } from "../utils";
 const middleware = (app) => {
   app.use(async (request, response, next) => {
     const token = request.headers["authorization"].split(" ")[1];
-    console.log(token);
     if (!token) {
       return sendResponse(onError(403, messageResponse.TOKEN_ERROR), response);
     } else {
       const verified = jwt.jwtVerify(token, config.SECRET);
-      if (verified) {
+      if (verified){
         next();
       }
     }
