ec05c1039eae29e6514a54b3a2e4bd0b9aab1e66
Sameer Srivastava <sameer.srivastava@fiftyfivetech.io>
2023-07-20 17:18:15 +0530

    updated meal route

--- src/core/routes/protected/meal.js
+++ src/core/routes/protected/meal.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/me", mealController.bookYourMeal);
 router.post("/multiple", mealController.bookMultipleMeals);
 router.delete("/me/delete", mealController.cancelMeal);
-router.get("/me/counts", isAdmin(mealController.getCountsOfUser));
+router.get("/me/counts", mealController.getCountsOfUser);
 router.post("/date/count", isAdmin(mealController.getAllCountOfDate));
 router.get("/week/count", isAdmin(mealController.getLastFiveCounts));
 
