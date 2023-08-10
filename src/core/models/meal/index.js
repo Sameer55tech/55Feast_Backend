c71c3598dccbc229ffb58a61bc7daf12730e8b3b
Sameer Srivastava <sameer.srivastava@fiftyfivetech.io>
2023-08-10 12:54:25 +0530

    added monthly backup

--- src/core/models/meal/index.js
+++ src/core/models/meal/index.js
@@ -1,9 +1,7 @@
 import mealModel from "./meal.model.js";
 import monthlyMealModel from "./monthlyMeal.model.js";
-import mealBackupModel from "./mealBackup.model.js";
 
 export default {
   mealModel,
   monthlyMealModel,
-  mealBackupModel
 };
